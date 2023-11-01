409bdf7c9491a0b0d5bea6a24d5270b3b5d4bba4
nazayca <nazayca@gmail.com>
2023-11-01 16:19:05 -0500

    changes added

--- js-loops/01-loops-intro.js
+++ js-loops/01-loops-intro.js
@@ -1,15 +1,51 @@
-for(let i = 10; i<=15; i++){
+
+// for loop
+/*
+num value       boolean condition       log value
+1               true                    1
+2               true                    2
+3               true                    3
+4               true                    4
+5               true                    5
+6               false
+*/
+
+for(let i = 10; i <= 15; i++) {
     console.log(i);
 }
 
-console.log("rest of the code");
+console.log('More code here!!!');
 
-for(let i = 1; i<=20;i++){
-    console.log("HelloWorld",i)
+/*
+10
+11
+12
+13
+14
+15
+More code here!!!
+*/
 
+// Print 'Hello World' 20 times
+for(let i = 1; i <= 20; i++) {
+    console.log('Hello World');
 }
 
-for (let i = 13; i<=18; i++){
+/*
+Log all the numbers below
+13
+14
+15
+16
+17
+18
+*/
+
+for(let i = 13; i <= 18; i++) {
     console.log(i);
 }
 
+for( ; ; ) {
+    console.log('Hello World');
+    if(2 === 2) break;
+}
